78ba789052becbeebf1a08987493097859a6f32a
kimgsun <gsune@kakao.com>
2025-10-13 05:28:33 +0900

    feat: stage1 종이 단서 update

--- assets/js/stage01/paper.js
+++ assets/js/stage01/paper.js
@@ -1,9 +1,6 @@
 export function handlePaper(modalTitle, puzzleContent, puzzleInput, submitBtn) {
     modalTitle.textContent = '거꾸로 쓰인 종이';
-    puzzleContent.innerHTML = `
-        <p style="font-size: 1.1rem; color: #a6d8ff; text-align: center; margin-bottom: 1rem;">거울에 비춰보면 뭔가 보일지도...</p>
-        <img src="../img/종이.png" alt="거꾸로 쓰인 종이" style="max-width: 100%; height: auto; display: block; margin: 0 auto;">
-    `;
+    puzzleContent.innerHTML = `<p>거울에 비춰보면 뭔가 보일지도...</p>`;
     puzzleInput.style.display = 'none';
     submitBtn.style.display = 'none';
 }
