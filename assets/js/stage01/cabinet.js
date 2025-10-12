7b8899cc7c1f2614cb59d864d63a70db87fbb33d
kimgsun <gsune@kakao.com>
2025-10-13 04:16:34 +0900

    feat: stage1 캐비넷 퍼즐 update

--- assets/js/stage01/cabinet.js
+++ assets/js/stage01/cabinet.js
@@ -1,9 +1,6 @@
-import puzzleManager from '../puzzles/puzzle-manager.js';
-
 export function handleCabinet(modalTitle, puzzleContent, puzzleInput, submitBtn) {
     modalTitle.textContent = '캐비넷';
     puzzleContent.innerHTML = `<p>잠겨있는 캐비넷이다. 무언가 특별한 방법으로 열어야 할 것 같다.</p>`;
-    setTimeout(() => {
-        puzzleManager.show('cabinet-puzzle', '캐비넷');
-    }, 0);
+    puzzleInput.style.display = 'none';
+    submitBtn.style.display = 'none';
 }
